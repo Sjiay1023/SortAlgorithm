64fcf0b73a1ab78ec7458b3b63f9345cfdedc98f
Sjiayu <1132676182@qq.com>
2015-11-24 18:26:34 +0800

    添加到版本库中

---
+++ sort.cpp
@@ -0,0 +1,32 @@
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+#include "node.h"
+#include "sort.h"
+using namespace std;
+struct list
+{
+	int key;	
+}
+//所有插入排序
+//直接插入排序
+//temp 相当于是一个监视哨 ，先将第一个记录看为有序，然后插入其他序列 
+void straight_insertion_sort(list *a)
+{
+	int len = a[0].key;
+	for(int i=2;i<=len;i++)
+	{
+		if(a[i].key <a[i-1].key)
+		{
+			int temp = a[i].key;
+			a[i].key = a[i-1].key;
+			int j;
+			for(j=i-2;j>0&&temp<(a[j].key);--j)
+			{
+				a[j+1].key = a[j].key;
+			}
+			a[j+1].key =temp;
+		}
+	} 
+		
+} 
