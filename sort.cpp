f9a95eebfafd3e6162f260a8252d34e87f45e928
Sjiayu <1132676182@qq.com>
2015-12-01 19:08:07 +0800

    增加堆排序和归并排序

--- sort.cpp
+++ sort.cpp
@@ -144,3 +144,75 @@ void select_sort(list *a)
 		swap(a[i].key,a[min_index].key);
 	}
 }
+
+//堆排序
+void heap_sort(list *a)
+{
+	int len = a[0].key;
+	//建立一个堆得过程
+	for(int i=len/2;i>0;--i)
+	{
+		heap_adjust(a,i,len);
+	} 
+	for(int i=len;i>1;--i)
+	{
+		swap(a[1].key,a[i].key);
+		heap_adjust(a,1,i-1);
+	}
+	
+} 
+void heap_adjust(list *a,int s,int m)
+{
+	//list a中出了a[s]之外的关键字均满足堆的定义
+	//本函数调整a[s]的关键字，是a[s....m]成为一个大顶堆
+	int temp =a[s].key;
+	for(int j=2*s;j<=m;j*=2)
+	{
+		if(j<m&&(a[j].key<a[j+1].key))++j;
+		if(temp>=a[j].key)break;
+		a[s].key=a[j].key;
+		s=j;
+	}
+	a[s].key=temp; 
+}
+
+
+//归并排序
+void merge_sort(list *a,int low,int high)
+{
+	int mid=((low+high)>>1);;
+	merge_sort(a,low,mid);
+	merge_sort(a,mid+1,high);
+	merge_sort(a,low,mid,high);	
+} 
+//归并排序的合并操作
+void merge_array(list *a,int low,int mid,int high)
+{
+	list *temp = new list[high-low+1+1];
+	int i=low,j=mid+1;
+	int index=0;
+	while(i<=mid&&j<=high)
+	{
+		if(a[i].key<a[j].key)
+		{
+			temp[index++].key=a[i++].key;
+		}
+		else
+		{
+			temp[index++].key=a[j++].key;
+		}
+	}
+	while(i<=mid)
+	{
+		temp[index++].key=a[i++].key;
+	}
+	while(j<=high)
+	{
+		temp[index++].key=a[j++].key;
+	}
+	for(i=0;i<index;++i)
+	{
+		a[low+i]=temp[i];
+	}
+	delete []temp;
+} 
